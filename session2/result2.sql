d1eb974076f5922359f17d627f63e15b28b5cf04
Fireblade75 <averink@gmail.com>
2014-05-06 15:13:48 +0200

    Exercise 2 p2

--- session2/result2.sql
+++ session2/result2.sql
@@ -2,3 +2,8 @@ INSERT INTO F ("ABC", "EF", false, "Aan de hand van ABC kan aan de hand van E-> 
 INSERT INTO F ("E", "D", true, "E is geen key dus het is een violation.");
 INSERT INTO F ("D", "A", true, "D is geen key dus het is een violation.");
 
+CREATE TABLE R1 (A INT, B INT, C, INT, E INT, F INT);
+CREATE TABLE R2 (
+	E INT, D INT
+	FOREIGN KEY (E) REFERENCES R1(E)  
+);
