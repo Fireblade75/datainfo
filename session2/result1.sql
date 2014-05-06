39de62f5cf0801d84c5d0f43cbc09a969a4c89a8
Fireblade75 <averink@gmail.com>
2014-05-06 14:45:57 +0200

    Exercise 1

---
+++ session2/result1.sql
@@ -0,0 +1,10 @@
+INSERT INTO Problem1 VALUES ('Á', false, "Meerdere voertuigen kunnen de zelfde capaciteit hebben.");
+INSERT INTO Problem1 VALUES ('B', true, "Een voertuig kan maar een capaciteit hebben.");
+INSERT INTO Problem1 VALUES ('C', true, "Een factuur hoort maar bij een pakketje.");
+INSERT INTO Problem1 VALUES ('D', true, "Een Pakket kan maar aan één afleveradres en factuur gekoppeld zijn.");
+INSERT INTO Problem1 VALUES ('E', true, "Het geldbedrag en ontvanger zijn gekoppeld aan de factuur die gekoppeld is aan het pakketje.");
+INSERT INTO Problem1 VALUES ('F', false, "Een bezorger kan gerechtigd zijn voor meerdere voertuigen.");
+INSERT INTO Problem1 VALUES ('G', false, "Meerdere bezorgers kunnen gerechtigd zijn voor het zelfde voertuig. De capaciteit is een eigenschap van het voertuig en verandert hier niets aan.");
+INSERT INTO Problem1 VALUES ('H', true, "Een bezorger heeft een vast voertuig op moment R, maar op een ander moment kan een andere bezorger dit voertuig gebruiken.");
+INSERT INTO Problem1 VALUES ('I', false, "Een voertuig heeft een vaste capaciteit.");
+INSERT INTO Problem1 VALUES ('J', true, "Een bezorger beïnvloed de capaciteit niet.");
