959f30a25ea55cf1ef768279103107a2b3b52ba0
Fireblade75 <averink@gmail.com>
2014-05-27 16:13:28 +0200

    ex

---
+++ session5/exercise2b.sql
@@ -0,0 +1,18 @@
+--Opdracht 2b
+
+--read1(x)
+SELECT titel FROM Boek WHERE isbn = '0136067018';
+--read2(x)
+SELECT titel FROM Boek WHERE isbn = ' 0136067018';
+--write1(x)
+UPDATE Boek SET titel = titel || ' deel 1' WHERE isbn = '0136067018';
+--read1(y)
+SELECT titel FROM Boek WHERE isbn = '0321228383';
+--write1(y)
+UPDATE Boek SET titel = titel || ' deel 1' WHERE isbn = '0321228383';
+--commit1
+COMMIT
+--write2(x)
+UPDATE Boek SET titel = titel || ' deel 2' WHERE isbn = ' 0136067018';
+--commit2
+COMMIT
